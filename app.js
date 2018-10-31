2726f96100071602414185aa7570f2e678309b5c
Alex <alex@address.com>
2018-10-31 22:29:01 +0100

    Installed Helmet for better security

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var helmet = require('helmet');
 
 var indexRouter = require('./routes/v1/index');
 var currenciesRouter = require('./routes/v1/currenciesRoutes');
@@ -19,6 +20,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(helmet());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/v1/', indexRouter);
