3d4c2fc32addf5e9b9e60376945244cb772e6a0a
Alex <alex@address.com>
2018-11-01 22:40:30 +0100

    api service: cleanup

--- expenses-api/controllers/currencyController.js
+++
@@ -1,9 +0,0 @@
-exports.getAll = async (req, res) => {
-    const currencies = new Array();
-
-    return res.status(201).json( {
-        status : "ok",
-        error : null,
-        data : currencies
-    });
-}
--- expenses-api/test/services/v1/getCurrencies.test.js
+++
@@ -1,9 +0,0 @@
-var assert = require('assert');
-var currencyController = require('../../../controllers/currencyController');
-var expect = require('chai');
-
-describe('currencies', () => {
-    it ('should not be empty', () => {
-        assert.equal(true, false);
-    })
-});
